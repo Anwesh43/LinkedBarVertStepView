e8b5ea08e0f93a6fe541bbb9ed007ae5760bcd64
Anwesh43 <anweshthecool0@gmail.com>
2018-10-05 08:54:48 +0530

    adding libraries and defining nodes

--- barvertstepview/src/main/java/com/anwesh/uiprojects/barvertstepview/BarVertStepView.kt
+++ barvertstepview/src/main/java/com/anwesh/uiprojects/barvertstepview/BarVertStepView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.barvertstepview
 /**
  * Created by anweshmishra on 05/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+
+val nodes : Int = 5
