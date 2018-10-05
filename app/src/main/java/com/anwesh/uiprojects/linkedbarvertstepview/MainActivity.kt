b2c88a7224b9e93ecd550749ca28cce27dd35757
Anwesh43 <anweshthecool0@gmail.com>
2018-10-05 09:55:58 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbarvertstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbarvertstepview/MainActivity.kt
@@ -2,10 +2,12 @@ package com.anwesh.uiprojects.linkedbarvertstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.barvertstepview.BarVertStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        BarVertStepView.create(this)
     }
 }
